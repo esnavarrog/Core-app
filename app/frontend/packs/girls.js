d34e4dfd07d072ede159ae8615c24c975126f479
Esteban Navarro <esnavarrog@gmail.com>
2021-11-08 18:43:36 -0300

    Add loader

--- app/frontend/packs/girls.js
+++ app/frontend/packs/girls.js
@@ -1,6 +1,15 @@
 $(document).on('turbolinks:load', events)
 
 function events() {
+    function loader(){
+        setTimeout(function() {
+            $('.loader').fadeOut(1000);
+            $('#body').removeClass('hidden-loader');
+        }, 1000);
+    }
+
+    $('window').on('load', loader());
+
     $('body').on('change', '#valor-consultar', function () {
         let checkbox = $(this);
         if (checkbox.is(":checked")) {
