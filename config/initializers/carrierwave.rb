7d6054441c2cf448aa6d6c8347f68ff645804925
Esteban Navarro <esnavarrog@gmail.com>
2021-11-09 15:03:13 -0300

    fixed fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,9 @@ if Rails.env.production?
             # Configuration for Amazon S3
             :provider               => 'AWS',
             :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
-            :use_iam_profile        => true, 
             :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
             :region                 => ENV['S3_REGION']
         }
         config.fog_directory     =  ENV['S3_BUCKET']
-        config.fog_public     = false                                                 # optional, defaults to true
-        config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
     end
 end
