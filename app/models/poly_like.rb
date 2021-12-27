2908902a43fb252f4ca2ba47a14f1ee8bd2bf1a2
Esteban Navarro <esnavarrog@gmail.com>
2021-12-27 03:51:43 -0300

    change icons for gifs

--- app/models/poly_like.rb
+++ app/models/poly_like.rb
@@ -1,5 +1,5 @@
 class PolyLike < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true
-  enum emoji: [:heart_arrow, :devil_smile, :enamorado, :eye_heart, :eye_star, :kiss, :wink, :fire]
+  enum emoji: [:heart_arrow, :devil_smile, :enamorado, :angry, :wink, :kiss]
 end
