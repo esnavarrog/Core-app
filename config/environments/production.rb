f2cecb281a4afdfb9dfd5ff0214f43345d545c37
Esteban Navarro <esnavarrog@gmail.com>
2022-05-06 11:41:56 -0400

    remove line of forse_ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.force_ssl = true
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
