d33e6210d8d0c8934b1de99ade3c63d9d99e8c94
Esteban Navarro <esnavarrog@gmail.com>
2022-05-09 19:44:03 -0400

    fixed scroll infitnite

--- app/frontend/packs/posts.js
+++ app/frontend/packs/posts.js
@@ -9,28 +9,28 @@ function events() {
 
     // pagination
     $(window).on('scroll', function(){
-        if ($(window).scrollTop() + window.innerHeight == $(document).height()){
+        if ($(window).scrollTop() + $(window).height() == $(document).height()){
             console.log('listo')
             pagination("pc")
         }
     })
 
     $('window').on('touchmove', function() {
-        if ($(window).scrollTop() + window.innerHeight == $(document).height()){
-            console.log('listo molbil')
+        if ($(window).scrollTop() + $(window).height() == $(document).height()){
             pagination("mobil")
         }
     });
 
-    function pagination(displau){
+    function pagination(display){
         let url = $('#paginate-posts .pagination .next a').attr('href');
+        console.log(display)
         if(url){
             $.ajax({
                 type: "GET",
                 url: url,
                 dataType: "script",
                 data:{
-                    data:displau
+                    data:display
                 }
             });
         }else{
