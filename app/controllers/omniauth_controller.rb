80352eb65bf3fda5c87df883a0e6d6547835d1de
Esteban Navarro <esnavarrog@gmail.com>
2022-05-09 16:57:02 -0400

    change omniauth for google

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -1,31 +1,21 @@
 class OmniauthController < ApplicationController
 
     def facebook
-        @user = User.create_from_provider_data(request.env["omniauth.auth"])
-        if @user.persisted?
-            flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Facebook'
-            sign_in_and_redirect @user, event: :authentication
-        else
-            session['devise.facebook_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-            redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-        end
+        handle_auth "Facebook"
     end
 
     def google_oauth2
-        # You need to implement the method below in your model (e.g. app/models/user.rb)
-        @user = User.create_from_provider_data(request.env['omniauth.auth'])
-
-        if @user.persisted?
-            flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
-            sign_in_and_redirect @user, event: :authentication
-        else
-            session['devise.google_data'] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
-            redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
-        end
+        handle_auth "Google"
     end
     
-    def failure
-        flash[:error] = 'Tenemos un problema para iniciar sesión. Por favor regístrate o vuelve a iniciar sesión mas tarde.'
-        redirect_to new_user_registration_path
+    def handle_auth(provider)
+        @user = User.from_omniauth(request.env['omniauth.auth'])
+        if @user.persisted?
+            flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: provider.capitalize
+            sign_in_and_redirect @user, event: :authentication
+        else
+            session['devise.auth_data'] = request.env['omniauth.auth'].except('extra')
+            redirect_to new_user_registration_url, alert: user.errors.full_messages.join("\n")
+        end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,13 +17,21 @@ class User < ApplicationRecord
 
 
 
-  def self.create_from_provider_data(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.nickname = auth.info.name # assuming the user model has a name
-      user.avatar = auth.info.image # assuming the user model has an image
+  def self.from_omniauth(access_token)
+
+    user = User.where(email: access_token.info.email).first
+    unless user
+      user = User.create(
+        email: access_token.info.email,
+        password: Devise.friendly_token[0,20]
+      )
     end
+    user.name = access_token.info.name
+    user.image = access_token.info.image
+    user.uid = access_token.uid
+    user.provider = access_token.provider
+    user.save
+    user
   end
 
 end
