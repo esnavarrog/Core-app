19aeeeb7961902a17bfc99147f087d860c209d8c
Esteban Navarro <esnavarrog@gmail.com>
2022-05-10 13:05:03 -0400

    fixed omniauth

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -15,11 +15,12 @@ class OmniauthController < ApplicationController
             sign_in_and_redirect @user, event: :authentication
         else
             session['devise.auth_data'] = request.env['omniauth.auth'].except('extra')
-            redirect_to new_user_registration_url, alert: user.errors.full_messages.join("\n")
+            redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
         end
     end
 
     def failure
-        redirect_to :root, alert: user.errors.full_messages.join("\n")
+        @user = User.from_omniauth(request.env['omniauth.auth'])
+        redirect_to :root, alert: @user.errors.full_messages.join("\n")
     end
 end
