0c7c0e32ab990f066f9b98cb4a294971b6f5a1a2
Esteban Navarro <esnavarrog@gmail.com>
2022-06-04 03:44:10 -0400

    not production

--- app/models/webpay_plus.rb
+++ app/models/webpay_plus.rb
@@ -1,5 +1,6 @@
 class WebpayPlus
-	URL_BASE = Rails.env.production? ? 'https://webpay3g.transbank.cl' : 'https://webpay3gint.transbank.cl'
+	# URL_BASE = Rails.env.production? ? 'https://webpay3g.transbank.cl' : 'https://webpay3gint.transbank.cl'
+	URL_BASE = 'https://webpay3gint.transbank.cl'
 
 	def self.init_transaction(order)
 		unless order.respond_to?(:webpay_object_data)
